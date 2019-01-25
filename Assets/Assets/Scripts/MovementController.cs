1f1e4bb510d98cd61cdc25838b2ee3d1b62a2874
Cem Sahin <cem.sahin19@gmail.com>
2019-01-26 01:02:59 +0300

    Scene

--- Assets/Assets/Scripts/MovementController.cs
+++ Assets/Assets/Scripts/MovementController.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class MovementController : MonoBehaviour
+public class MovementController : MonoBehaviour, IPointerClickHandler
 {
+    //SmoothDamp Variables
     private float smoothTime = 0.5f;
-
+    private float maxSpeed = 1f;
     private Vector3 targetPosition;
     private Vector3 currentPosition;
     private Vector3 speed = Vector3.zero;
+
+    // Double Click Variables
+    private float doubleClickTimeLimit = 0.25f;
+
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(InputListener());
         currentPosition = transform.position;
         targetPosition = currentPosition;
     }
@@ -20,15 +27,14 @@ public class MovementController : MonoBehaviour
     void Update()
     {
         currentPosition = transform.position;
-        if (targetPosition == currentPosition + new Vector3(0.01f, 0.01f, 0.01f))
+        if (targetPosition == currentPosition)
             return;
         else
-            transform.position = Vector3.SmoothDamp(currentPosition, targetPosition, ref speed, smoothTime);
+            transform.position = Vector3.SmoothDamp(currentPosition, targetPosition, ref speed, smoothTime, maxSpeed);
     }
 
     void OnGUI()
     {
-
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
@@ -36,11 +42,50 @@ public class MovementController : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit))
             {
-                Transform objectHit = hit.transform;
-                Debug.Log(objectHit.transform.position.x);
-                targetPosition = new Vector3(objectHit.position.x, transform.position.y, transform.position.z);
+                Vector3 objectPoint = hit.point;
+                targetPosition = new Vector3(objectPoint.x, transform.position.y, transform.position.z);
+                transform.LookAt(targetPosition);
             }
         }
 
     }
+    private IEnumerator InputListener()
+    {
+        while (enabled)
+        {
+            if (Input.GetMouseButtonDown(0))
+                yield return ClickEvent();
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator ClickEvent()
+    {
+        maxSpeed = 1f;
+        yield return new WaitForEndOfFrame();
+
+        float count = 0f;
+        while (count < doubleClickTimeLimit)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                DoubleClick();
+                yield break;
+            }
+            count += Time.deltaTime;
+            yield return null; 
+        }
+    }
+
+    private void DoubleClick()
+    {
+        maxSpeed = 2f;
+    }
+
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        Debug.Log("asdsa");
+    }
+
 }
