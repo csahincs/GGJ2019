7fd3ff0edbe2340e19c73f953e473bf572d49713
Cem Sahin <cem.sahin19@gmail.com>
2019-01-26 18:11:56 +0300

    Main Menu / Loading Screen

--- Assets/Scripts/MainMenuButtons.cs
+++ Assets/Scripts/MainMenuButtons.cs
@@ -4,22 +4,22 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour
 {
-    public Button startBtn;
+    //public Button startBtn;
     public Button optionsBtn;
     public Button exitBtn;
 
 
     public void Start()
     {
-        startBtn.onClick.RemoveAllListeners();
-        startBtn.onClick.AddListener(StartBtnClicked);
+        //startBtn.onClick.RemoveAllListeners();
+        //startBtn.onClick.AddListener(StartBtnClicked);
         exitBtn.onClick.RemoveAllListeners();
         exitBtn.onClick.AddListener(ExitBtnClicked);
     }
 
     private void StartBtnClicked()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene(1);
     }
 
     private void OptionsBtnClicked()
