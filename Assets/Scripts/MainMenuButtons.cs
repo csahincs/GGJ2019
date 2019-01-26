555677d84b7149f8256df2673b713517de15e80e
Cem Sahin <cem.sahin19@gmail.com>
2019-01-26 17:07:52 +0300

    Added Main Menu

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public Button startBtn;
+    public Button optionsBtn;
+    public Button exitBtn;
+
+
+    public void Start()
+    {
+        startBtn.onClick.RemoveAllListeners();
+        startBtn.onClick.AddListener(StartBtnClicked);
+        exitBtn.onClick.RemoveAllListeners();
+        exitBtn.onClick.AddListener(ExitBtnClicked);
+    }
+
+    private void StartBtnClicked()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    private void OptionsBtnClicked()
+    {
+
+    }
+
+    private void ExitBtnClicked()
+    {
+        Application.Quit();
+    }
+}
