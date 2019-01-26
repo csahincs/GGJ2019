03e09bc28c5d632a23a3e45e6644ec0dba6d8445
Kadir Cenk Alpay <kadircenkalpay@gmail.com>
2019-01-26 14:28:05 +0300

    added hud map anim

--- Assets/Assets/Scripts/UIMapIconFunctions.cs
+++ Assets/Assets/Scripts/UIMapIconFunctions.cs
@@ -4,25 +4,32 @@ using UnityEngine.EventSystems;
 public class UIMapIconFunctions : MonoBehaviour, IPointerClickHandler
 {
     GameObject map;
-    private bool flag;
+    Animator mapAnimator;
+    private bool turnOnOff;
+    public float speedMult = 2.0f;
 
     private void Start()
     {
         map = GameObject.Find("UIMap");
+        mapAnimator = map.GetComponent<Animator>();
         map.SetActive(false);
     }
 
     public void OnPointerClick(PointerEventData data)
     {
-        if (flag)
+        if (turnOnOff)
         {
-            map.SetActive(false);
-            flag = false;
+            //map.SetActive(false);
+            mapAnimator.SetFloat("speed", -speedMult);
+            mapAnimator.Play("fadein");
+            turnOnOff = false;
         }
         else
         {
             map.SetActive(true);
-            flag = true;
+            mapAnimator.SetFloat("speed", speedMult);
+            mapAnimator.Play("fadein");
+            turnOnOff = true;
         }
     }
 }
