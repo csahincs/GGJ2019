9b5947ac8b0de7a19d332bdd075ce22e3880f0d2
Kadir Cenk Alpay <kadircenkalpay@gmail.com>
2019-01-26 12:39:08 +0300

    added UI map icon prefab and scripts

---
+++ Assets/Assets/Scripts/UIMapIconFunctions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIMapIconFunctions : MonoBehaviour, IPointerClickHandler
+{
+    GameObject map;
+    private bool flag;
+
+    private void Start()
+    {
+        map = GameObject.Find("UIMap");
+        map.SetActive(false);
+    }
+
+    public void OnPointerClick(PointerEventData data)
+    {
+        if (flag)
+        {
+            map.SetActive(false);
+            flag = false;
+        }
+        else
+        {
+            map.SetActive(true);
+            flag = true;
+        }
+    }
+}
