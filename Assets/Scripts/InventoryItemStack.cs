53bbd7ff738b88c6ad2fe6d8639e5339a0ee758e
Cem Sahin <cem.sahin19@gmail.com>
2019-01-27 02:53:43 +0300

    Inventory Feature

---
+++ Assets/Scripts/InventoryItemStack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class InventoryItemStack : MonoBehaviour
+{
+    public Image icon;
+    public TextMeshProUGUI itemCountTxt;
+    public void Initialize(Sprite itemIcon, int count)
+    {
+            this.icon.sprite = itemIcon;
+            itemCountTxt.text = count.ToString();
+    }
+}
